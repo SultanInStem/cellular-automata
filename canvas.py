1092c21af67ed58f811cd30e21e92d1fc511687a
= <sultaninstem004@gmail.com>
2025-01-13 20:57:03 -0800

    minor

--- canvas.py
+++ canvas.py
@@ -13,3 +13,20 @@ class Canvas:
             if event.type == pygame.QUIT: 
                 self.running = False
 
+    def update(self): 
+        pass 
+
+    def render(self): 
+        self.screen.fill((0,0,0))
+
+        pygame.display.flip()
+        self.clock.tick(60)
+
+    def run(self): 
+        while(self.running): 
+            self.handle_events()
+            self.update()
+            self.render()
+        pygame.quit()
+        sys.exit()
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-
+from canvas import Canvas
 if __name__ == "__main__": 
+    Main_Canvas = Canvas((1200,800))
+    Main_Canvas.run()
     print("name")
