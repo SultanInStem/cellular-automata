7f5ff607a175d047c6cc6f1df56101920972a69e
= <sultaninstem004@gmail.com>
2025-01-13 18:43:57 -0800

    setu[

--- canvas.py
+++ canvas.py
@@ -1,9 +1,15 @@
 import pygame 
-
+import sys 
 
 class Canvas: 
     def __init__(self, size): 
         pygame.init()
         self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Cellular Automata")
-        
+        self.clock = pygame.time.Clock()
+        self.running = True
+    def handle_events(self): 
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                self.running = False
+
