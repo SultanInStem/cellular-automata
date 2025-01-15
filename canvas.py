d49409c1daae994ea44e437e0f736e4099fc3316
= <sultaninstem004@gmail.com>
2025-01-14 19:25:52 -0800

    not finished indexing

--- canvas.py
+++ canvas.py
@@ -11,15 +11,15 @@ class Canvas:
         self.clock = pygame.time.Clock()
         self.fps = 30
         self.running = True
-        self.is_paused = False
+        self.is_paused = True
         self.grid = []
-        cell_height = SCREEN_SIZE[1] // ROWS
-        cell_width = SCREEN_SIZE[0] // COLS
-        cell_size = (cell_height, cell_width)
+        self.cell_width = SCREEN_SIZE[0] // COLS
+        self.cell_height = SCREEN_SIZE[1] // ROWS
+        cell_size = (self.cell_height, self.cell_width)
         for r in range(0, ROWS): 
             temp_row = []
             for c in range(0, COLS):
-                pos = (r * cell_width, c * cell_height)
+                pos = (r * self.cell_width, c * self.cell_height)
                 temp_row.append(Cell(pos, False, cell_size))
             self.grid.append(temp_row)    
         
@@ -29,10 +29,19 @@ class Canvas:
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and self.is_paused: 
+                ### if the game is paused, resurrect a cell at the mouse position
+                mouse_pos = pygame.mouse.get_pos()
+                ### modulate the positions to get array index of the cell 
+                ## FINISH UP WORKING ON THIS !!!
+                self.grid[mouse_pos[1] % ROWS][mouse_pos[0] % COLS].resurrect()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_SPACE]: 
+                self.is_paused = not self.is_paused
 
     def update(self): 
-        if self.is_pause: pass
-
+        if self.is_paused:
+            pass
 
 
     def render(self): 
